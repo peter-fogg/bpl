8b3c52c39b875e512a23f8f0e96de93078726292
peter-fogg <peter.fogg@oberlin.edu>
2014-02-18 15:23:33 -0500

    Move types into a separate module.

--- BPL/Scanner.hs
+++ BPL/Scanner.hs
@@ -1,59 +1,12 @@
 module BPL.Scanner
-       ( TokenType(..)
-       , LineNumber
-       , Token(..)
-       , tokenize
+       (
+         tokenize
        )  where
 
 import Data.Char (isSpace, isAlpha, isDigit)
 import qualified Data.Map as Map
 
-data TokenType = TkIdentifier
-               | TkStringLiteral
-               | TkNumber
-               | TkString
-               | TkInt
-               | TkVoid
-               | TkIf
-               | TkElse
-               | TkWhile
-               | TkReturn
-               | TkWrite
-               | TkWriteLn
-               | TkRead
-               | TkSemicolon
-               | TkComma
-               | TkLSquare
-               | TkRSquare
-               | TkLCurly
-               | TkRCurly
-               | TkLParen
-               | TkRParen
-               | TkRAngle
-               | TkLAngle
-               | TkLEQ
-               | TkGEQ
-               | TkDoubleEqual
-               | TkNotEqual
-               | TkSingleEqual
-               | TkPlus
-               | TkMinus
-               | TkStar
-               | TkSlash
-               | TkPercent
-               | TkAmpersand
-               | TkEOF
-               deriving (Eq, Show)
-
-type LineNumber = Integer
-
-data Token = Token { tokenType :: TokenType
-                   , value :: String
-                   , line :: LineNumber
-                   } deriving (Eq)
-
-instance Show Token where
-  show (Token t v l) = show t ++ " \"" ++ v ++ "\"" ++ " : (line " ++ show l ++ ")"
+import BPL.Types
 
 reservedWords :: Map.Map String TokenType
 reservedWords = Map.fromList [ ("string", TkString)
---
+++ BPL/Types.hs
@@ -0,0 +1,52 @@
+module BPL.Types
+       (TokenType(..)
+       , LineNumber
+       , Token(..)
+       ) where
+
+data TokenType = TkIdentifier
+               | TkStringLiteral
+               | TkNumber
+               | TkString
+               | TkInt
+               | TkVoid
+               | TkIf
+               | TkElse
+               | TkWhile
+               | TkReturn
+               | TkWrite
+               | TkWriteLn
+               | TkRead
+               | TkSemicolon
+               | TkComma
+               | TkLSquare
+               | TkRSquare
+               | TkLCurly
+               | TkRCurly
+               | TkLParen
+               | TkRParen
+               | TkRAngle
+               | TkLAngle
+               | TkLEQ
+               | TkGEQ
+               | TkDoubleEqual
+               | TkNotEqual
+               | TkSingleEqual
+               | TkPlus
+               | TkMinus
+               | TkStar
+               | TkSlash
+               | TkPercent
+               | TkAmpersand
+               | TkEOF
+               deriving (Eq, Show)
+
+type LineNumber = Integer
+
+data Token = Token { tokenType :: TokenType
+                   , value :: String
+                   , line :: LineNumber
+                   } deriving (Eq)
+
+instance Show Token where
+  show (Token t v l) = show t ++ " \"" ++ v ++ "\"" ++ " : (line " ++ show l ++ ")"
